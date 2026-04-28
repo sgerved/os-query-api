ca4d2eb9eb87be98af39404722bce5a5a948b7ce
sga <s.gerved@gerved.eu>
2026-04-28 16:58:27 +0200

    Add initial Entity Framework implementation with `ApiEventsDbContext` and `ApiEventModel`; create initial database migration. Update `.gitignore` to exclude SQLite database files.

---
+++ os-query-api/DataAccess/ApiEventsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using os_query_api.DataAccess.Models;
+
+namespace os_query_api.DataAccess;
+
+public class ApiEventsDbContext : DbContext
+{
+    public DbSet<ApiEventModel> ApiEvents { get; set; }
+    
+    private string DbPath { get; set; }
+    
+    public ApiEventsDbContext()
+    {
+        var directory = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(directory);
+        DbPath = Path.Combine(path, "api-events.db");
+    }
+    
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
---
+++ os-query-api/Migrations/20260428145542_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace os_query_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApiEvents",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EventName = table.Column<string>(type: "TEXT", nullable: false),
+                    EventData = table.Column<string>(type: "TEXT", nullable: false),
+                    EventTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiEvents", x => x.EventId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiEvents");
+        }
+    }
+}
